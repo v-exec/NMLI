e3792a260f8a9ae900b0cba57741f286580a081b
vi <reducedprocess@gmail.com>
2019-07-03 19:03:41 +0900

    make music player smaller, fix decrypted data formatting

--- scripts/story_data.js
+++ scripts/story_data.js
@@ -1,38 +1,57 @@
-decryptions.push(`
+decryptions.push(`File contents:
+
 Oh, you! Human!
-<br>
-<br>
-I know, you must be shocked: a large explosion just happened, things are looking awfully frightful, and now a little piece of plastic is writing on your screen. I assure you, this is at least <i>somewhat normal</i>, and you’ll be <i>fine</i>. I’ve accessed the camera on your little computer here so we can have a conversation face to face, as you humans would!
-<br>
-<br>
+
+I know, you must be shocked: a large explosion just happened, things are looking awfully frightful, and now a little piece of plastic is writing on your screen. I assure you, this is at least *somewhat normal*, and you’ll be *fine*. I’ve accessed the camera on your little computer here so we can have a conversation face to face, as you humans would!
+
 Ah, I haven’t introduced myself, have I? I am the light!
-<br>
-<br>
+
 I suppose we can start with what happened here in the first place. Well, I was moved to this park after a heavy storm some hundred years ago. The sight was dreadful! Flipped over benches and outdoor dining tables, a couple of damaged cars, and even a house roof which had made a lovely acquaintance with a large tree branch. I used to live on the streets of London, and then I upgraded to the Northeast end of this area some century ago. Not a terrible progression, as I enjoyed the calmer scenery and generally more pleasant allure to the area’s visitors. Besides, I could do my job even better here! You’d think far too few people visit in comparison to a bustling street, but what matters is not the quantity, but the potential for influence. Everyone’s so busy on the streets, nobody even notices you. How am I supposed to help when I can’t even compete with all the attention-grabbing propaganda plastered everywhere?
-<br>
-<br>
-Oh,<i> right</i>, I suppose you were more curious about the explosion. To tell you the truth, I’ve no clue what happened. But I can reckon a guess, if you care for speculation.
+
+Oh, *right*, I suppose you were more curious about the explosion. To tell you the truth, I’ve no clue what happened. But I can reckon a guess, if you care for speculation.
 `);
 
-decryptions.push(`
+decryptions.push(`File contents:
+
 Oh, you’re walking away? I suppose I’ll just keep writing so you have something to read once you’re back. Now then, where was I... Oh yes!
-<br>
-<br>
+
 You see, I am the light. To be more specific, a lamp. Yes, a literal lamp. The type you would put on a street to light up the sidewalk. ‘You’re awfully smart-sounding for a lamp’, is what I imagine you thinking right now, and you’re absolutely right, I am at least marginally smarter than a standard lamp.
-<br>
-<br>
+
 I, along with virtually all lamps installed in the 1800s and 1900s, was programmed by what you would refer to as ‘extra-terrestrials’. This was all part of a plan thought up by the big artificial intelligence they worship. Turns out, superintelligent AI is actually nowhere near as dangerous as you’d think. Once it was up and running, it just got all lazy and unproductive until someone asks it to do something. Some say it’s a lucky error in the code, others say it’s existential dread.
-<br>
-<br>
+
 Anyhow, the big boys up top asked the great AI: “Oh great AI, we must commence grooming the human race to ensure the safety of the world. How do we accomplish this?”. And the AI was like “make a bunch of sentient lamps and put them on Earth”. To which the aliens, naturally, replied “what?”, and then the AI said “you heard me”, and the aliens said “o-okay?”, and then they did it.
-<br>
-<br>
-Thing is, the AI has a tendency to come up with weird plans that somehow always work out. It’s been doing this for centuries, so there’s not much doubt in its plans. But, I mean, it’s <i>theoretically</i> possible that it’s just been unbelievably, miraculously lucky. Anyways, the lamps on this planet began to use their extremely limited form of expression: turning on or off, as their way of interacting with humanity. Us lamps are... I guess you could say ‘altruistic’? We enjoy helping out. The other day I blinked on and off rapidly to wake up a sleepy driver. I mean, I don’t know if it worked, but I’m sure I’ve made <i>some</i> difference over the course of my life. And all my other lamp friends? There’s no telling what the cumulative effects of our actions could have done. But they’ve been gone for a while now. The AI lamps that were previously installed have been renovated by humans with regular, non-sentient lamps. I was installed in an old lamppost, so they kept me for ‘historical value’. As far as I can tell, I’m the only one left.
-<br>
-<br>
+
+Thing is, the AI has a tendency to come up with weird plans that somehow always work out. It’s been doing this for centuries, so there’s not much doubt about its abilities. Though, I mean, it’s *theoretically* possible that it’s just been unbelievably, miraculously lucky. Anyways, the lamps on this planet began to use their extremely limited form of expression: turning on or off, as their way of interacting with humanity. Us lamps are... I guess you could say ‘altruistic’? We enjoy helping out. The other day I blinked on and off rapidly to wake up a sleepy driver. I mean, I don’t know if it worked, but I’m sure I’ve made *some* difference over the course of my life. And all my other lamp friends? There’s no telling what the cumulative effects of our actions could have done. But they’ve been gone for a while now. The AI lamps that were previously installed have been renovated by humans with regular, non-sentient lamps. I was installed in an old lamppost, so they kept me for ‘historical value’. As far as I can tell, I’m the only one left.
+
 As for the whole ‘grooming’ thing, I’m not sure what they have in mind. That information’s sort of on the down-low. As far as I remember, though, they’ll be establishing first contact with you on 14.11.2141. My date timer was damaged when I was moved to the park, but I think that’s pretty close.
 `);
 
-decryptions.push(`
-a=k b=f c=x d=n e=c f=i g=a h=t i=z j=j k=l l=m m=h n=d o=u p=y q=v r=p s=e t=g u=q v=o w=r x=w y=b z=s
+decryptions.push(`File contents:
+
+a=k
+b=f
+c=x
+d=n
+e=c
+f=i
+g=a
+h=t
+i=z
+j=j
+k=l
+l=m
+m=h
+n=d
+o=u
+p=y
+q=v
+r=p
+s=e
+t=g
+u=q
+v=o
+w=r
+x=w
+y=b
+z=s
 `);
